fc4643c11433c8e7c9b3dd520424e558465eebbb
lp <1027204291@qq.com>
2017-08-25 18:06:11 +0800

    分支切换测试2

--- app/src/main/java/com/showtime/lp/fragment/example/ExampleRightFragment.java
+++ app/src/main/java/com/showtime/lp/fragment/example/ExampleRightFragment.java
@@ -31,6 +31,11 @@ public class ExampleRightFragment extends BaseFragment {
             parent.removeView(view);
         }
 
+
+
+
+
+
         Log.e("right--onCreateView--", "--------------");
 
         initView();
